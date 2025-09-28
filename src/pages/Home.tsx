15673150b092bf462b31653f49ce95c0f2a62781
Mark_Jeong <81726531+markjung96@users.noreply.github.com>
2025-09-28 14:54:54 +0900

    chore(deps): update dependencies and remove unused home page

--- src/pages/Home.tsx
+++
@@ -1,9 +0,0 @@
-import { ButtonExamples } from '@/components/ui/ButtonExamples'
-
-export default function Home() {
-  return (
-    <div className="text-gray-900">
-      <ButtonExamples />
-    </div>
-  )
-}
