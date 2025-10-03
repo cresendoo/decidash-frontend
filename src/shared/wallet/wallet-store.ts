b944bd5d2c80a2109ed8cea34dac3afc199ae85a
coldbell <qkrckswhd200@naver.com>
2025-10-04 00:54:50 +0900

    feat(wallet): initialize Zustand store for wallet management

--- src/shared/wallet/wallet-store.ts
+++ src/shared/wallet/wallet-store.ts
@@ -1,7 +1,9 @@
+import { create } from 'zustand';
+
 export type Wallet = { type: string };
 
-
-export const useWallet = create<Wallet>((set) => ({
+// TODO: 여기 어케하지
+export const useWallet = create<Wallet>(() => ({
     type: 'mock',
     address: '0x0',
     seed: '0x0',
